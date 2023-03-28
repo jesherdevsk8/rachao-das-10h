e2b05a5305f7baa88e819d303f3cf0cfba2af848
Jesher Minelli Alves <jesherdevsk8@gmail.com>
2023-03-28 13:08:02 -0300

    fix: add has_many validation

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,5 @@
 class Player < ApplicationRecord
+  has_many :soccer_matchs
+
   validates :email, :name, presence: true
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
+  describe 'associations' do
+    it { is_expected.to have_many(:soccer_matchs) }
+  end
+
   describe 'validations' do
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_presence_of(:name) }
