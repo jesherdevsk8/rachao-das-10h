7b65f3b050d936876faea70425a8ff23d4a2ffe2
Jesher Minelli Alves <jesherdevsk8@gmail.com>
2023-03-28 13:54:40 -0300

    add some tests

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,8 @@ class Player < ApplicationRecord
   has_many :soccer_matchs
 
   validates :email, :name, presence: true
+  validates :email, uniqueness: true, format: {
+    with: /\A^[a-z0-9._]+@[a-z0-9]+\.[a-z]{2,3}+(\.[a-z]{2,3})?(\.[a-z]{2})?$\z/,
+    message: 'must be a valid email address'
+  }
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -6,7 +6,9 @@ RSpec.describe Player, type: :model do
   end
 
   describe 'validations' do
+    subject { create(:player) }
     it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
     it { is_expected.to validate_presence_of(:name) }
   end
 end
