4d6fee7555784c83b70be7cbe77ec4d956c714e1
Jesher Minelli Alves <jesherdevsk8@gmail.com>
2023-03-27 21:54:09 -0300

    config parameter_sanitizer if devise_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+  validates :email, uniqueness: true, format: {
+    with: /\A^[a-z0-9._]+@[a-z0-9]+\.[a-z]{2,3}+(\.[a-z]{2,3})?(\.[a-z]{2})?$\z/,
+    message: 'must be a valid email address'
+  }
 end
